ee59e59318768ba9af60287caa02ec09b8a518d9
Benjamin DE BOISSIEU <bendeb@hotmail.com>
2018-09-08 20:42:45 +0200

    with componentWillMount to get the movie list

--- src/containers/app.js
+++ src/containers/app.js
@@ -1,15 +1,35 @@
-import React from 'react'
+import React, {Component} from 'react'
 import SearchBar from '../components/search-bar'
 import VideoList from './video-list'
+import axios from 'axios'
 
-const App =  () => {
-  return (
-    <div>
-      <SearchBar />
-      <VideoList />
+const API_KEY = "741b59d51974b2f81350ac6e7e2f4b79";
 
-    </div>
-    )
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {movieList:{}, currentMovie:{}}
+  }
+
+  componentWillMount(){
+    axios.get(`${API_END_POINT}${POPULAR_MOVIES_URL}&${API_KEY}`).then(function(response){
+      console.log("---");
+      console.log('',response);
+      console.log("---")
+      this.setState({movieList:response.data.results.slice(1,6)});
+      this.setState({currentMovie:response.data.results[0]});
+    }.bind(this));
+  }
+  render() {
+            return (
+              <div>
+                <SearchBar />
+                <VideoList />
+
+              </div>
+              )
+
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import App from './containers/app'
 
 
-const API_KEY = "741b59d51974b2f81350ac6e7e2f4b79";
+
 
 
 ReactDOM.render(<App/>, document.querySelector('.container'));
