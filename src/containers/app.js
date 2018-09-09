f2f2b3797e045afdb680487cdcb3695b73671d79
Benjamin DE BOISSIEU <bendeb@hotmail.com>
2018-09-09 19:27:42 +0200

    with video detail display title and description

--- src/containers/app.js
+++ src/containers/app.js
@@ -1,9 +1,13 @@
 import React, {Component} from 'react'
 import SearchBar from '../components/search-bar'
+import VideoDetail from '../components/video-detail'
 import VideoList from './video-list'
 import axios from 'axios'
 
-const API_KEY = "741b59d51974b2f81350ac6e7e2f4b79";
+const API_END_POINT = "https://api.themoviedb.org/3/";
+const POPULAR_MOVIES_URL = "discover/movie?sort_by=popularity.desc&include_adult=false&append_to_response=images";
+
+const API_KEY = "api_key=741b59d51974b2f81350ac6e7e2f4b79";
 
 class App extends Component {
   constructor(props){
@@ -13,19 +17,20 @@ class App extends Component {
 
   componentWillMount(){
     axios.get(`${API_END_POINT}${POPULAR_MOVIES_URL}&${API_KEY}`).then(function(response){
-      console.log("---");
-      console.log('',response);
-      console.log("---")
       this.setState({movieList:response.data.results.slice(1,6)});
       this.setState({currentMovie:response.data.results[0]});
+      console.log('',this.state.currentMovie);
+      console.log("---");
+      console.log('',this.state.movieList);
+      console.log("---")
     }.bind(this));
   }
   render() {
             return (
               <div>
                 <SearchBar />
+                <VideoDetail title={this.state.currentMovie.title} description={this.state.currentMovie.overview} />
                 <VideoList />
-
               </div>
               )
 
