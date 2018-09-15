4d970ddc2260d7aa5de17ce793c485d4738156c7
Benjamin DE BOISSIEU <bendeb@hotmail.com>
2018-09-15 20:06:05 +0200

    add title.js

---
+++ src/components/title.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Title = () => {
+  return <div>
+          <h1>Watch a trailer</h1>
+          <p>By Benjamin de Boissieu</p>
+         </div>
+}
+
+export default Title;
