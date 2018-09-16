3c15a4ed3eeb69f6a12faa18fdb93edc07186425
Benjamin DE BOISSIEU <bendeb@hotmail.com>
2018-09-16 17:03:27 +0200

    with background details

--- src/components/video-detail.js
+++ src/components/video-detail.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const VideoDetail = ({title, description}) => {
   return (
-    <div>
+    <div className="details">
       <h1>{title}</h1>
       <p>{description}</p>
     </div>
